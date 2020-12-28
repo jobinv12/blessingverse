31aa0176e9fb0a85ac3fc59eb9223d3f55a433c3
Jobin Thavu Varghese <jobinthavuvarghese@Jobins-MacBook-Pro.local>
2020-12-28 22:35:38 +0530

    v0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+## Steps to Follow
+
+# Install nodejs and npm
+# download the file and cd into it
+# open terminal and type npm i
+
+## To run the application
+
+# nodemon app.js
+
+# if not working install nodemon
+# npm i -g nodemon
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const PORT = process.env.PORT | 5000
+
+app.engine('handlebars',exphbs({defaultLayout:"main"}))
+app.set('view engine','handlebars')
+
+app.use("/static",express.static(path.resolve(__dirname,"static")))
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
