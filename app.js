678ebb81ba1b18c59ef3a16a174c115d502df6a3
Jobin Thavu Varghese <jobinthavuvarghese@Jobins-MacBook-Pro.local>
2020-12-29 10:21:24 +0530

    v0.1

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.listen(PORT, ()=> console.log(`Server Running on port ${PORT}`))
